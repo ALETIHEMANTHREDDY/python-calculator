f9c93c6cb40629b2e04db0d9f53516f2922d87af
ALETI HEMANTH REDDY <hemanthreddya21@gmail.com>
2025-07-02 22:34:10 +0530

    Added my first Python script 🔥
    This is my first GitHub upload. Just getting started with Python.

---
+++ your_version_calculator.py
@@ -0,0 +1,14 @@
+# Your original code:
+
+print("babe,", "name")
+print("20,", "age")
+print("CBIT,", "college")
+print("chemical,", "branch")
+
+# Expected Output:
+"""
+babe, name
+20, age
+CBIT, college
+chemical, branch
+"""
